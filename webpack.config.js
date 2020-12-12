4eac37717ab19f5584b68747b2dbcd0c777bd5a6
papaAndrew <a.hermit71@gmail.com>
2020-12-12 14:49:31 +0500

    setup webpack and html-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,11 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  // ...
+  entry: "./src/index.js",
+  output: {
+    filename: "index_bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     compress: true,
