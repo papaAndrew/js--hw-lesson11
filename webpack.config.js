4510c2fc74ea008153ea41700c39e4d9c432049a
papaAndrew <a.hermit71@gmail.com>
2020-12-12 22:34:26 +0500

    setup webpack, babel, server

--- webpack.config.js
+++ webpack.config.js
@@ -12,5 +12,19 @@ module.exports = {
     compress: true,
     port: 9000,
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
   plugins: [new HtmlWebpackPlugin()],
 };
